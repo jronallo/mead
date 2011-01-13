c27bb578f38ff233eed0a82b581ec3ca7cad63eb
Jason Ronallo <jronallo@gmail.com>
2011-01-12 20:55:52 -0500

    update tests to use tilde

--- test/test_validations.rb
+++ test/test_validations.rb
@@ -8,11 +8,11 @@ class TestMeadValidations < Test::Unit::TestCase
     pp mead.errors
   end
 
-  context "validating meads" do  
+  context "validating meads" do
     setup do
       @loc_ua023_031 = File.open('test/ead/ua023_031.xml')
       @loc_mc00240 = File.open('test/ead/mc00240.xml')
-    end  
+    end
 
     should "show ua023_031-001-cb0003-005-001 to be a valid mead" do
       mead = Mead::Identifier.new('ua023_031-001-cb0003-005-001', @loc_ua023_031)
@@ -23,7 +23,7 @@ class TestMeadValidations < Test::Unit::TestCase
       mead = Mead::Identifier.new('ua023_031-001-cb0013-019A-001', @loc_ua023_031)
       assert mead.valid?
     end
-    
+
     should "show ua023_031-001-cb0013-019A-001_0002 to be a valid mead" do
       mead = Mead::Identifier.new('ua023_031-001-cb0013-019A-001', @loc_ua023_031)
       assert mead.valid?
@@ -38,14 +38,14 @@ class TestMeadValidations < Test::Unit::TestCase
       mead = Mead::Identifier.new('ua023_031-002-cb0006-031-001', @loc_ua023_031)
       assert_equal false, mead.valid?
     end
-    
+
     should "show ua023_031-002-cb0006-031-001 to be well-formed" do
       mead = Mead::Identifier.new('ua023_031-002-cb0006-031-001', @loc_ua023_031)
       assert mead.valid_format?
     end
 
     should "show ua023_031-001-cb0156-031-001 to be well-formed but invalid" do
-      mead = Mead::Identifier.new('ua023_031-002-cb0126-031-001', @loc_ua023_031)      
+      mead = Mead::Identifier.new('ua023_031-002-cb0126-031-001', @loc_ua023_031)
       assert_equal false, mead.valid?
     end
 
@@ -59,17 +59,17 @@ class TestMeadValidations < Test::Unit::TestCase
       #inspect_validation(mead)
       assert mead.valid?
     end
-    
+
     should "use the cached metadata in the Mead::Identifier instead of making a new extraction request" do
       mead = Mead::Identifier.new('mc00240-001-ff0052-000-001_0002', @loc_mc00240).extract
       assert mead.valid?
     end
-    
+
     should "show mc00240-002-bx0061-fr77_23,1-001 to be a well-formed mead" do
-      mead = Mead::Identifier.new('mc00240-002-bx0061-fr77,23_1-001', @loc_mc00240).extract
+      mead = Mead::Identifier.new('mc00240-002-bx0061-fr77~23_1-001', @loc_mc00240).extract
       assert mead.valid?
     end
-    
+
     should 'show mc00240-003-bx0068-fr002-001 to be a valid mead' do
       mead = Mead::Identifier.new('mc00240-003-bx0068-fr002-001', @loc_mc00240).extract
       assert mead.valid?
@@ -78,3 +78,4 @@ class TestMeadValidations < Test::Unit::TestCase
   end
 
 end
+
